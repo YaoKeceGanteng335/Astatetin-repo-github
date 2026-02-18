579b6da75070b44304d92ec404bee52a96a9a6f1
YaoKeceGanteng335 <igustiagarjuna67@gmail.com>
2026-02-18 12:56:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuicon = document.getElementById("menu-icon");
+const menulist = document.getElementById("menu-list");
+
+menuicon.addEventListener("click" , () => {
+    menulist.classList.toggle("hidden");
+        alert("hey kamu klck icon!");
+
+});
